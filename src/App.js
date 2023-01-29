13d1e81693dbaec8de5d69e9977bd3d56f969e04
John Benedict Geli <john.benedict.geli.ipi@prulifeuk.com.ph>
2023-01-30 01:33:34 +0800

    created login page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+import Login from './component/login';
+import { useEffect } from 'react';
 
 function App() {
+  // function handleCallbackResponse(response){
+
+
+  //   console.log("Encoded JWT ID token: " + response.credentials);
+    
+  // useEffect(() => {
+  //     google.accounts.id.initialize({
+  //       client_id: "890779726484-2hrboltbt3id8s3o0cl1r76pj6e5v3vd.apps.googleusercontent.com",
+  //       callback: handleCallbackResponse
+  //     });
+  //     google.accounts.id.renderButton(
+  //       document.getElementById("SignInDiv"),
+  //       { theme: "outline", size: "large"}
+  //     );
+  // }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Login/>
+    <div id="signInDev"></div>
     </div>
+    
   );
 }
 
